76b9126fcbebf73e86432444f8c2f643be88fb94
Ionel <dobre_ionel@yahoo.com>
2018-09-30 21:48:33 +0300

    MangoDB connect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import Lmv from './lmv.js'
-
+import { 
+    Stitch,
+    RemoteMongoClient,
+    AnonymousCredential
+} from "mongodb-stitch-browser-sdk";
 
 import './App.css';
 
@@ -146,7 +150,22 @@ class App extends Component {
 
  
   render() {
-    return (
+const client = Stitch.initializeDefaultAppClient('talentati-rtvdi');
+
+const db = client.getServiceClient(RemoteMongoClient.factory, 'mongodb-atlas').db('talent');
+
+client.auth.loginWithCredential(new AnonymousCredential()).then(user => 
+  db.collection('talentati').updateOne({owner_id: client.auth.user.id}, {$set:{number:42}}, {upsert:true})
+).then(() => 
+  db.collection('talentati').find({owner_id: client.auth.user.id}, { limit: 100}).asArray()
+).then(docs => {
+    console.log("Found docs", docs)
+    console.log("[MongoDB Stitch] Connected to Stitch")
+}).catch(err => {
+    console.error(err)
+});
+      return (
+      
       <div className="App">
        <Lmv
        locuri={this.state.super}
